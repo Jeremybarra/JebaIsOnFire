09ee44f250978dc37429745614146d1c7dc8cfe9
cocosar07 <cocosar07370@gmail.com>
2016-07-06 01:52:01 +0200

    [REF] Changement structure code

--- constantes.py
+++ constantes.py
@@ -2,6 +2,8 @@
 """Constantes de Fire Emblem"""
 
 #Paramètres de la fenêtre
+sprite_width = 50
+sprite_height = 50
 nombre_sprite_cote = 18
 nombre_ligne = 15
 taille_sprite = 50
@@ -12,7 +14,7 @@ hauteur_fenetre = nombre_ligne * taille_sprite
 titre_fenetre = "Fire Emblem"
 image_icone = "Images/myrmidon_droite.PNG"
 image_myrmidon = "Images/myrmidon_droite.PNG"
-image_curseur = "Images/curseur.png"
+image_curseur = "Images/curseur2.png"
 
 #Listes des images du jeu
 image_accueil = "Images/accueil.png"
