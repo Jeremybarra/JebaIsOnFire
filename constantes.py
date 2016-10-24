683caf09fc1466164746a6e6361bac0e7e8f01e8
Jerem <barra.jeremy@hotmail.com>
2016-10-24 20:53:43 +0200

    [REF] Certaines constantes sont remises à leurs places respectives

--- constantes.py
+++ constantes.py
@@ -8,15 +8,15 @@ sprite_height = 50
 #Personnalisation de la fenêtre
 titre_fenetre = "Fire Emblem"
 image_icone = "Images/myrmidon_droite.PNG"
-image_myrmidon = "Images/myrmidon_droite.PNG"
-image_curseur = "Images/curseur2.png"
 
 #Listes des images du jeu
+image_curseur = "Images/curseur2.png"
 image_accueil = "Images/accueil.png"
 image_fond = "Images/accueil.png"
 image_plaine = "Images/plaine.PNG"
 image_foret ="Images/foret.PNG"
 image_fort ="Images/fort.PNG"
+image_myrmidon = "Images/myrmidon_droite.PNG"
 image_mouvements_possibles = "Images/case_accessible2.png"
 
 #Constantes liées à la partie
